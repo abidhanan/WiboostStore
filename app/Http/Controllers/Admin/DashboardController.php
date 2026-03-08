66e46b4de3cbaf8db14f69af88f17de274431466
Abid Hanan <abidhanan0904@gmail.com>
2026-03-08 10:40:03 +0700

    Admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    /**
+     * Menampilkan halaman utama Dashboard Admin.
+     */
+    public function index()
+    {
+        // Memanggil file view yang sudah kita buat tadi di resources/views/admin/dashboard.blade.php
+        return view('admin.dashboard');
+    }
 }
